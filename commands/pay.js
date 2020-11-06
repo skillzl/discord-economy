b8a68fb9d0d44bb10ab116beb97df5b87c60d67e
skillzl <45903404+skillzl@users.noreply.github.com>
2020-11-06 17:52:31 +0200

    🌟 modified commands

--- commands/pay.js
+++ commands/pay.js
@@ -2,28 +2,28 @@ const Discord = require("discord.js");
 const db = require("quick.db");
 const ms = require("parse-ms");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
 
   let user = message.mentions.members.first() || message.author;
 
   let member = db.fetch(`money_${message.author.id}`)
 
   if (!user) {
-      return message.channel.send(`<:tik:701192592741761164> \`${user.username}\`, mention someone to give credits.`)
+      return message.channel.send(`Wrong usage, mention someone to give credits.`)
   }
   
   if (!args[1]) {
-      return message.channel.send(`<:tik:701192592741761164> \`${user.username}\`, specify an amount to give credits.`)
+      return message.channel.send(`Wrong usage, specify an amount to give credits.`)
   }
   if (message.content.includes('-')) { 
-      return message.channel.send(`<:tik:701192592741761164> \`${user.username}\`, you can't pay someone negative credits.`)
+      return message.channel.send(`Wrong usage, you can't pay someone negative credits.`)
   }
 
   if (member < args[1]) {
-      return message.channel.send(`<:tik:701192592741761164> \`${user.username}\`, you don't have that much credits.`)
+      return message.channel.send(`Wrong usage, you don't have that much credits.`)
   }
 
-  message.channel.send(`<:tik:701192592741761164> You gived to: \`${user.user.username}\` \`${args[1]}\` credits.`)
+  message.channel.send(`You transfered \`${args[1]}\` credits to **${user.user.username}**'s balance.`)
   db.add(`money_${user.id}`, args[1])
   db.subtract(`money_${message.author.id}`, args[1])
 
@@ -31,5 +31,5 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name:"pay",
-  aliases: ["pay"]
+  aliases: ["transfer", "givemoney"]
 }  
