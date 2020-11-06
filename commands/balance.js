ff56b1772868ea0378c899fc767ba28a01ca7c4f
skillzl <45903404+skillzl@users.noreply.github.com>
2020-11-06 17:44:33 +0200

    🌟 modified commands

--- commands/balance.js
+++ commands/balance.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
 
-module.exports.run = async (bot, message, args, utils, iUser) => {
+module.exports.run = async (client, message, args) => {
 
   let user = message.mentions.members.first() || message.author;
   const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
@@ -10,11 +10,11 @@ module.exports.run = async (bot, message, args, utils, iUser) => {
 
   if (bal === null) bal = 0;
 
-  message.channel.send(`<:tik:701192592741761164> ★ | Here is \`${member.user.tag}\` balance:\n\n**★ Name**: \`${member.user.tag}\`.\n**★ Balance**: \`${bal}\` credits.`)
+  message.channel.send(`**${member.user.tag}** \`${bal}\` credits.`)
 }; 
 
 
 module.exports.help = {
   name:"balance",
-  aliases: ["bal"]
+  aliases: ["bal", "credits", "money"]
 }
