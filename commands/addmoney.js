7379d730ba2322b76b7c0145e5379fd85bbf4024
skillzl <45903404+skillzl@users.noreply.github.com>
2020-11-06 17:43:17 +0200

    🌟 modified commands

--- commands/addmoney.js
+++ commands/addmoney.js
@@ -1,12 +1,10 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
 
-exports.run = async (bot, message, args) => {
+exports.run = async (client, message, args) => {
    const ownerID = [
-    "503983707502411787",
-    "659038301331783680",
-    "592528540444917760",
-    "331345768759820289"
+    "botOwner",
+    "botOwner"
   ];
   if (!ownerID.includes(message.author.id)) return;
 
@@ -15,11 +13,11 @@ exports.run = async (bot, message, args) => {
     if (isNaN(args[1])) return;
     db.add(`money_${user.id}`, args[1])
     let bal = await db.fetch(`money_${user.id}`)
-    message.channel.send(`<:tik:701192592741761164> ★ Added \`${args[1]}\` credits now **${user}** have \`${bal}\` credits**.**`)
+    message.channel.send(`Added \`${args[1]}\` credits to **${user}**'s balance.\n> Current balance: \`${bal}\` credits.`)
 
 };
 
 module.exports.help = {
   name:"addmoney",
-  aliases: ["am"]
+  aliases: ["addcredits"]
 }
